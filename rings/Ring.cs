7b0346064724ecd8003057b727a438f6d9175748
VladimirVlasov <vpvlasov@edu.hse.ru>
2019-04-13 12:44:20 +0300

    Add console ring app

---
+++ rings/Ring.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace rings
+{
+    public class Ring
+    {
+        private int RingBase { get; set; }
+        private int[,] SumsTable { get; set; }
+        private int[,] ProdsTable { get; set; }
+
+        public Ring(int ring_base)
+        {
+            RingBase = ring_base;
+            SumsTable = new int[RingBase, RingBase];
+            ProdsTable = new int[RingBase, RingBase];
+
+            for (int i = 0; i < RingBase; i++)
+            {
+                for (int j = 0; j < RingBase; j++)
+                {
+                    SumsTable[i, j] = (i + j) % RingBase;
+                    ProdsTable[i, j] = (i * j) % RingBase;
+                }
+            }
+        }
+
+        public void print_sums()
+        {
+            Console.Write("\t");
+            for (int i = 0; i < RingBase; i++)
+            {
+                Console.Write(i.ToString() + "\t");
+            }
+            Console.Write("\n");
+
+            for (int i = 0; i < RingBase; i++)
+            {
+                Console.Write(i.ToString());
+                for (int j = 0; j < RingBase; j++)
+                {
+                    Console.Write("\t" + SumsTable[i, j].ToString());
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+
+        public void print_prods()
+        {
+            Console.Write("\t");
+            for (int i = 0; i < RingBase; i++)
+            {
+                Console.Write(i.ToString() + "\t");
+            }
+            Console.Write("\n");
+
+            for (int i = 0; i < RingBase; i++)
+            {
+                Console.Write(i.ToString());
+                for (int j = 0; j < RingBase; j++)
+                {
+                    Console.Write("\t" + ProdsTable[i, j].ToString());
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+
+        public bool is_field()
+        {
+            bool is_field = true;
+
+            for (int i = 1; i < RingBase; i++)
+            {
+                for (int j = 1; j < RingBase; j++)
+                {
+                    if (ProdsTable[i, j] == 0)
+                    {
+                        is_field = false;
+                    }
+                }
+            }
+
+            return is_field;
+        }
+    }
+}
---
+++ rings/rings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace rings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! I am here to help you with rings!");
+            Console.WriteLine("Rules are very simple! You will write num\n" +
+                "and I will write table of sums and prods in\n" +
+                "modular ring of this num.");
+
+            bool number_inputed = false;
+            int modular = 0;
+            while (!number_inputed)
+            { 
+                number_inputed = Int32.TryParse(Console.ReadLine(), out modular);
+                if (!number_inputed)
+                {
+                    Console.WriteLine("Try again!");
+                }
+            }
+
+            Ring my_ring = new Ring(modular);
+            my_ring.print_sums();
+            my_ring.print_prods();
+            if (my_ring.is_field()) 
+            {
+                Console.WriteLine("Looks like field...");
+            }
+            else
+            {
+                Console.WriteLine("Ohhhh, no field there.");
+            }
+        }
+    }
+}
